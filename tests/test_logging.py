b258ed51de557480ad756001cd0e18714f53aca6
ccdale <chris.allison@hivehome.com>
2021-07-09 10:44:12 +0100

    toy test for logging

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -5,4 +5,7 @@ log = ptvg.log
 
 def test_logmessage():
     log.error("this is an error message")
+    log.warning("this is a warning message")
+    log.info("this is an info message")
+    log.debug("this is a debug message")
     assert 1 == 1
